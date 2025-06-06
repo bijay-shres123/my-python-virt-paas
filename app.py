d3143833a1315f5da08210af30cea226dea0556a
bijay-shres123 <bijay.shres123@gmail.com>
2025-06-06 13:45:30 +0545

    return

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    reurn "Autocommit Test 303  PaaS!"
+    return "Autocommit Test 303  PaaS!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
