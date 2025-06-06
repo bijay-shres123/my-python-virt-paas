3717d377c354e73cbae6aa7a8643b7d6b46b9e86
bijay-shres123 <bijay.shres123@gmail.com>
2025-06-06 13:14:15 +0545

    helloWORLD

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Set fallback port
 PORT=${PORT:-7979}
+
+# Kill old gunicorn processes
+pkill -f "gunicorn wsgi:app"
+
+# Small delay to allow cleanup
+sleep 1
+
+# Start new gunicorn process
 exec python3.13 -m gunicorn wsgi:app --bind 0.0.0.0:$PORT
 
