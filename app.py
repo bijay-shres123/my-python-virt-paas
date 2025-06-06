e214b41aa3e2c7dee38a0cd5162dc3702a213b33
bijay-shres123 <bijay.shres123@gmail.com>
2025-06-06 11:28:28 +0545

    changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Pratham PaaS!"
+    return "Hello from Pratham BOSS  PaaS!"
 
 if __name__ == '__main__':
     app.run()
